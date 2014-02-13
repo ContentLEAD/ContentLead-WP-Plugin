551ca34a333a4aefa389abfa391589dd5792f6bf
Ali <alimf@bc.edu>
2014-02-13 15:53:29 -0500

    Testing git plugin updater

--- BraftonWordpressPlugin.php
+++ BraftonWordpressPlugin.php
@@ -18,11 +18,11 @@ function brafton_plugin_github_updater(){
   if (is_admin()) { // note the use of is_admin() to double check that this is happening in the admin
       $config = array(
           'slug' => plugin_basename(__FILE__), // this is the slug of your plugin
-          'proper_folder_name' => 'ContentLead-WP-Plugin', // this is the name of the folder your plugin lives in
+          'proper_folder_name' => 'ContentLead-WP-Plugin-fixed-images', // this is the name of the folder your plugin lives in
           'api_url' => 'https://api.github.com/repos/ContentLEAD/ContentLead-WP-Plugin', // the github API url of your github repo
           'raw_url' => 'https://github.com/ContentLEAD/ContentLead-WP-Plugin/tree/fixed-images', // the github raw url of your github repo
           'github_url' => 'https://github.com/ContentLEAD/ContentLead-WP-Plugin', // the github url of your github repo
-          'zip_url' => 'https://github.com/ContentLEAD/ContentLead-WP-Plugin/zipball/fixed-images.zip', // the zip url of the github repo
+          'zip_url' => 'https://github.com/ContentLEAD/ContentLead-WP-Plugin/archive/fixed-images.zip', // the zip url of the github repo
           'sslverify' => true, // wether WP should check the validity of the SSL cert when getting an update, see https://github.com/jkudish/WordPress-GitHub-Plugin-Updater/issues/2 and https://github.com/jkudish/WordPress-GitHub-Plugin-Updater/issues/4 for details
           'requires' => '2.9', // which version of WordPress does your plugin require?
           'tested' => '3.8', // which version of WordPress is your plugin tested up to?
